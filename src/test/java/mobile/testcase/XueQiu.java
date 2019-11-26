b4bd0e8bf2c396308d549da38bb21475cf04eed9
liuqiceshi <1432435771@qq.com>
2019-11-26 19:19:52 +0800

    模拟器模拟发送短信和打电话，以及设备信息

--- src/test/java/mobile/testcase/XueQiu.java
+++ src/test/java/mobile/testcase/XueQiu.java
@@ -1,6 +1,7 @@
 package mobile.testcase;
 
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.GsmCallActions;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import mobile.page.App;
@@ -9,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
 
 import java.time.Duration;
 
@@ -19,6 +21,39 @@ public class XueQiu extends BasePage {
         App.start();
         driver.findElement(By.id("com.xueqiu.android:id/image_cancel")).click();
     }
+
+    /**
+     * 模拟器模拟发送短信和打电话
+     */
+    @Test
+    public void testCall(){
+        driver.sendSMS("13141322817","hello liuqi");
+        driver.makeGsmCall("13141322817", GsmCallActions.CALL);
+
+    }
+
+
+    /**
+     * 获取设备信息
+     */
+    @Test
+    public void testDevice() throws InterruptedException {
+        driver.runAppInBackground(Duration.ofSeconds(5));
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+        Thread.sleep(3000);
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+        driver.openNotifications();
+        Thread.sleep(3000);
+        driver.manage().logs().getAvailableLogTypes().forEach(x->System.out.println(x));
+        System.out.println(driver.getBatteryInfo().getState().ordinal());
+        System.out.println(driver.getPerformanceData("com.xueqiu.android","cpuinfo",5));
+
+    }
+
+    /**
+     * 屏幕滑动
+     * @throws InterruptedException
+     */
     @Test
     public void testSwipe() throws InterruptedException {
 
