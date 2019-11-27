1ab2471dc44caf66bdec382739c5517e8f845c32
liuqiceshi <1432435771@qq.com>
2019-11-27 18:35:50 +0800

    在Capability中添加unicodeKeyboard和resetKeyboard为true，键盘可以输入中文

--- src/test/java/mobile/page/App.java
+++ src/test/java/mobile/page/App.java
@@ -17,18 +17,25 @@ public class App extends BasePage {
         desiredCapabilities.setCapability("deviceName", "emulator-5554");
         desiredCapabilities.setCapability("appPackage","com.xueqiu.android");
         desiredCapabilities.setCapability("appActivity",".view.WelcomeActivityAlias");
+        //Enable Unicode input, default false
+        desiredCapabilities.setCapability("unicodeKeyboard","true");
+        //Reset keyboard to its original state, after running Unicode tests with unicodeKeyboard capability. Ignored if used alone. Default false
+        desiredCapabilities.setCapability("unicodeKeyboard","true");
         //noRest值为true时缓存、权限被清空 ，false时保留缓存数据
         desiredCapabilities.setCapability("noRest",false);
         desiredCapabilities.setCapability("autoGrantPermissions",true);
+
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 
     }
 
     //进入搜索框
     public static SearchPage toSearch() {
        findEementAndClick(By.id("com.xueqiu.android:id/home_search"));
+       //driver.findElement(By.id("com.xueqiu.android:id/home_search")).click();
        return new SearchPage();
     }
 
